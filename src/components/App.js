cdc7c95cf180eb902734d7c013b8c441c459ef1a
pankajmokashi <pankajmokasi12@gmail.com>
2023-07-25 17:11:40 +0530

    submitting code-Tue Jul 25 2023 17:11:38 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,35 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  let [data, setData] = useState({username: "", password: ""})
+  let [error, setError] = useState("")
+
+  function handleChange(){
+    if(!data.username || !data.password){
+      setError("Both username and password are required.")
+    }
+    else{
+      setError("")
+    }
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <div>
+          <input type="text" onChange={(e) => setData({...data, "username": e.target.value})}></input>
+        </div>
+        <div>
+          <input type="password" onChange={(e) => setData({...data, "password": e.target.value})}></input>
+        </div>
+        {
+          error &&
+          <div id="errorMessage">{error}</div>
+        }
+        <button onClick={handleChange}>Login</button>
     </div>
   )
 }
